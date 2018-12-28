71d0a96e9f6635b06b7ffad52f64f2bec4ddbbd1
Tim Urista <timothy.urista@gmail.com>
2018-12-28 06:52:59 -0800

    add animation based on gesture for first card

--- expo-project/src/Deck.js
+++ expo-project/src/Deck.js
@@ -1,9 +1,38 @@
 import React, { Component } from "react";
-import { View, Animated, Text } from "react-native";
+import { View, Animated, Text, PanResponder } from "react-native";
 
 class Deck extends Component {
+  constructor(props) {
+    super(props);
+    this.position = new Animated.ValueXY();
+    this.panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => {
+        // excecuted anytime user presses on screen
+        // means user presses on screen
+        return true;
+      },
+      onPanResponderMove: (event, gesture) => {
+        // console.log(gesture);
+        // dx, dy is really what we care about
+        this.position.setValue({ x: gesture.dx, y: gesture.dy });
+      },
+      onPanResponderRelease: () => {}
+    });
+  }
   renderCards = () => {
-    return this.props.data.map(item => {
+    return this.props.data.map((item, index) => {
+      if (index === 0) {
+        return (
+          <Animated.View
+            key={item.id + "view"}
+            style={this.position.getLayout()}
+            {...this.panResponder.panHandlers}
+          >
+            {this.props.renderCard(item)}
+          </Animated.View>
+        );
+      }
+
       return this.props.renderCard(item);
     });
   };
