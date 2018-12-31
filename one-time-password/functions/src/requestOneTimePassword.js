30999612cf29570ee01ec8a06c600aab0e2cab84
Tim Urista <timothy.urista@gmail.com>
2018-12-31 05:52:19 -0800

    add one time password steps to integrate with database

--- one-time-password/functions/src/requestOneTimePassword.js
+++ one-time-password/functions/src/requestOneTimePassword.js
@@ -1,4 +1,5 @@
 const admin = require("firebase-admin");
+const twilio = require("./twilio");
 
 module.exports = (req, rese) => {
   if (!req.body.phone) {
@@ -11,6 +12,29 @@ module.exports = (req, rese) => {
     .auth()
     .getUser(phone)
     .then(userRecord => {
-      rese.send({ message: "sent text to user" });
-    });
+      // 4 digits between 0, 9999
+      const code = Math.floor(Math.random() * 8999 + 1000);
+
+      twilio.messages.create(
+        {
+          body: "Your code is " + code,
+          to: phone,
+          from: "+13238949531"
+        },
+        err => {
+          if (err) {
+            return res.status(500).send({ error: err });
+          }
+
+          // user model has finite set of properties
+          admin
+            .database()
+            .ref("users/" + phone)
+            .update({ code: code, codeValid: true }, () => {
+              res.send({ success: true });
+            });
+        }
+      );
+    })
+    .catch(err => res.status(422).send({ error: err }));
 };
