9810035af94e2d7540a40367bc7c246b26fecab3
Tim Urista <timothy.urista@gmail.com>
2019-01-02 06:42:15 -0800

    add async await axios post to create user and request password

--- expo-project/src/SignUpForm.js
+++ expo-project/src/SignUpForm.js
@@ -6,15 +6,38 @@ import {
   Button,
   FormValidationMessage
 } from "react-native-elements";
+import axios from "axios";
+import { ROOT_URL } from "../secrets/project_urls";
 
 class SignUpForm extends Component {
   state = { phone: "" };
+
+  handleSubmit = async () => {
+    console.log("submit pressed");
+    console.log(ROOT_URL, this.state.phone);
+
+    try {
+      await axios.post(`${ROOT_URL}/createUser`, {
+        phone: this.state.phone
+      });
+
+      await axios.post(`${ROOT_URL}/requestOneTimePassword`, {
+        phone: this.state.phone
+      });
+      console.log("REQ RES", requestedRes);
+    } catch (e) {
+      console.log(e);
+    }
+  };
   render() {
     return (
       <View style={{ marginBottom: 10 }}>
-        <FormLabel>text</FormLabel>
-        <FormInput />
-        <Button title="Submit" />
+        <FormLabel>Enter Phone Number</FormLabel>
+        <FormInput
+          value={this.state.phone}
+          onChangeText={phone => this.setState({ phone })}
+        />
+        <Button title="Submit" onPress={this.handleSubmit} />
       </View>
     );
   }
